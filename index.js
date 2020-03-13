814fefb1a85f3064329b34da16ea1719d09a4c20
Anže Sršen <srsen.anze@gmail.com>
2020-03-13 14:00:41 +0100

    Remove sameSite, if secure is false

--- index.js
+++ index.js
@@ -610,11 +610,6 @@ function hash(sess) {
  */
 
 function issecure(req, trustProxy) {
-  // socket is https server
-  if (req.connection && req.connection.encrypted) {
-    return true;
-  }
-
   // do not trust proxy
   if (trustProxy === false) {
     return false;
@@ -624,15 +619,7 @@ function issecure(req, trustProxy) {
   if (trustProxy !== true) {
     return req.secure === true
   }
-
-  // read the proto from x-forwarded-proto header
-  var header = req.headers['x-forwarded-proto'] || '';
-  var index = header.indexOf(',');
-  var proto = index !== -1
-    ? header.substr(0, index).toLowerCase().trim()
-    : header.toLowerCase().trim()
-
-  return proto === 'https';
+  return req.protocol === 'https';
 }
 
 /**
